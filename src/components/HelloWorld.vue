1cf0b5bc767f2ccfb642cc698999bcb7d2215820
euthanasiaaa <finally.lbwyss@gmail.com>
2024-04-21 17:50:59 +0300

    first changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,11 +9,11 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+
+    <h1 class="blue"> {{ msg }} </h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Это мыыыыыыы!!!
+
     </h3>
   </div>
 </template>
@@ -24,6 +24,7 @@ h1 {
   font-size: 2.6rem;
   position: relative;
   top: -10px;
+
 }
 
 h3 {
